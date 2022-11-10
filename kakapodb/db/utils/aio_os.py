6f71a44d43e93de1caff64debcfdf82ff2494ca9
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-11-10 11:00:38 +0100

    ADD: more os module sync to async functions

--- kakapodb/db/utils/aio_os.py
+++ kakapodb/db/utils/aio_os.py
@@ -3,3 +3,8 @@ import os
 from kakapodb.db.utils.aio_helper import sync_to_async
 
 access = sync_to_async(os.access)
+fstat = sync_to_async(os.fstat)
+getcwd = sync_to_async(os.getcwd)
+listdir = sync_to_async(os.listdir)
+mkdir = sync_to_async(os.mkdir)
+
