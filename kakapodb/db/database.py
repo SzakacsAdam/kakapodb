6466ea45912638936237aa87f6be0ef0bb026ed5
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-11-10 11:03:21 +0100

    ADD: database init

--- kakapodb/db/database.py
+++ kakapodb/db/database.py
@@ -1,2 +1,18 @@
+from os import getcwd
+from os.path import join
+from typing import Dict
+
+from kakapodb.db.collection import Collection
+from kakapodb.db.utils import DirectoryPath
+
+
 class Database:
-    ...
+    _DEFAULT_NAME: str = "default"
+    _DEFAULT_DIR_SRC: str = join(getcwd(), _DEFAULT_NAME)
+    src = DirectoryPath(create=True)
+
+    def __init__(self, src_dir: str = _DEFAULT_DIR_SRC) -> None:
+        self.src = src_dir
+        self.collections: Dict[str, Collection] = {
+
+        }
